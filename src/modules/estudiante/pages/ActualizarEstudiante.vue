d231b1b262136026650d1a0ec6bbf4120c9c29a2
LABORATORIO DE COMPUTADORAS FACULTAD <fing.labcom@uce.edu.ec>
2023-08-02 19:48:25 -0500

    taller36

---
+++ src/modules/estudiante/pages/ActualizarEstudiante.vue
@@ -0,0 +1,20 @@
+<template>
+    <DatosActualizarEstudiante/>
+  </template>
+  
+  <script>
+  
+  import DatosActualizarEstudiante from '../components/DatosActualizarEstudiante.vue';
+  
+  export default {
+  
+    components: {
+        DatosActualizarEstudiante
+    }
+  
+  }
+  </script>
+  
+  <style>
+  
+  </style>
